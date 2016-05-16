a6a5192396112e027d81d91ed724dfbaa11506a7
Michael McCune <msm@redhat.com>
2016-05-16 11:29:16 -0400

    update formatting of server handler

--- handlers/server.go
+++ handlers/server.go
@@ -1,9 +1,9 @@
 package handlers
 
 import (
-    middleware "github.com/go-openapi/runtime/middleware"
-    
-    "github.com/redhatanalytics/oshinko-rest/restapi/operations/server"
+	middleware "github.com/go-openapi/runtime/middleware"
+
+	"github.com/redhatanalytics/oshinko-rest/restapi/operations/server"
 )
 
 var appName string
@@ -11,7 +11,7 @@ var gitTag string
 
 // ServerResponse respond to the server info request
 func ServerResponse() middleware.Responder {
-    payload := server.GetServerInfoOKBodyBody{
-        Application: &server.GetServerInfoOKBodyApplication{Name: &appName, Version: &gitTag}}
-    return server.NewGetServerInfoOK().WithPayload(payload)   
+	payload := server.GetServerInfoOKBodyBody{
+		Application: &server.GetServerInfoOKBodyApplication{Name: &appName, Version: &gitTag}}
+	return server.NewGetServerInfoOK().WithPayload(payload)
 }
