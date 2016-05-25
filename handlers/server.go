da18342babdef0a3ed5db61d7e7667af326579eb
Michael McCune <msm@redhat.com>
2016-05-25 11:34:12 -0400

    remove dot import for version

--- handlers/server.go
+++ handlers/server.go
@@ -4,15 +4,15 @@ import (
 	middleware "github.com/go-openapi/runtime/middleware"
 
 	"github.com/redhatanalytics/oshinko-rest/restapi/operations/server"
-	. "github.com/redhatanalytics/oshinko-rest/version"
+	"github.com/redhatanalytics/oshinko-rest/version"
 )
 
 // ServerResponse respond to the server info request
 func ServerResponse() middleware.Responder {
-	version := GetVersion()
-	appname := GetAppName()
+	vers := version.GetVersion()
+	name := version.GetAppName()
 	payload := server.GetServerInfoOKBodyBody{
 		Application: &server.GetServerInfoOKBodyApplication{
-			Name: &appname, Version: &version}}
+			Name: &name, Version: &vers}}
 	return server.NewGetServerInfoOK().WithPayload(payload)
 }
