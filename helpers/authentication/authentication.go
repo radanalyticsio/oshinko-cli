e6df28e0027bb0f59277e7a6c297ce15dd62b2bd
Subin <subin@apache.org>
2016-06-13 19:54:22 +0530

    add serviceaccount authentication support

--- helpers/authentication/authentication.go
+++ helpers/authentication/authentication.go
@@ -1,26 +1,68 @@
 package authentication
 
 import (
-	"errors"
+	"net"
+
 	_ "github.com/openshift/origin/pkg/api/install"
 	"github.com/openshift/origin/pkg/client"
+	oclient "github.com/openshift/origin/pkg/client"
 	serverapi "github.com/openshift/origin/pkg/cmd/server/api"
+	"k8s.io/kubernetes/pkg/client/restclient"
 	kclient "k8s.io/kubernetes/pkg/client/unversioned"
-	"os"
+	"k8s.io/kubernetes/pkg/util/crypto"
+
+	"github.com/redhatanalytics/oshinko-rest/helpers/info"
 )
+
+func SAConfig() (*restclient.Config, error) {
+	//fetch proxy IP + port
+	host, err := info.GetKubeProxyAddress()
+	if err != nil {
+		return nil, err
+	}
+	port, err := info.GetKubeProxyPort()
+	if err != nil {
+		return nil, err
+	}
+	token, err := info.GetServiceAccountToken()
+	if err != nil {
+		return nil, err
+	}
+	tlsClientConfig := restclient.TLSClientConfig{}
+	CAFile := info.GetServiceAccountCAPath()
+	_, err = crypto.CertPoolFromFile(CAFile)
+	if err != nil {
+		return nil, err
+	} else {
+		tlsClientConfig.CAFile = CAFile
+	}
+
+	return &restclient.Config{
+		Host:            "https://" + net.JoinHostPort(host, port),
+		BearerToken:     string(token),
+		TLSClientConfig: tlsClientConfig,
+	}, nil
+}
 
 func GetKubeClient() (*kclient.Client, error) {
 
 	// If the configfile option has been
 	// set, use it to get a client otherwise use the
 	// serviceaccount for authentication
-	configfile := os.Getenv("OSHINKO_KUBE_CONFIG")
+	configfile := info.GetKubeConfigPath()
 	if configfile != "" {
 		client, _, err := serverapi.GetKubeClient(configfile)
 		return client, err
 	} else {
-		// TODO whatever we have to do here to get a client using the serviceaccount
-		return nil, errors.New("OSHINKO_KUBE_CONFIG env var is required at this time")
+		saConfig, err := SAConfig()
+		if err != nil {
+			return nil, err
+		}
+		client, err := kclient.New(saConfig)
+		if err != nil {
+			return nil, err
+		}
+		return client, err
 	}
 }
 
@@ -29,12 +71,19 @@ func GetOpenShiftClient() (*client.Client, error) {
 	// If the configfile option has been
 	// set, use it to get a client otherwise use the
 	// serviceaccount for authentication
-	configfile := os.Getenv("OSHINKO_KUBE_CONFIG")
+	configfile := info.GetKubeConfigPath()
 	if configfile != "" {
 		client, _, err := serverapi.GetOpenShiftClient(configfile)
 		return client, err
 	} else {
-		// TODO whatever we have to do here to get a client using the serviceaccount
-		return nil, errors.New("OSHINKO_KUBE_CONFIG env var is required at this time")
+		saConfig, err := SAConfig()
+		if err != nil {
+			return nil, err
+		}
+		client, err := oclient.New(saConfig)
+		if err != nil {
+			return nil, err
+		}
+		return client, err
 	}
 }
--- helpers/info/info.go
+++ helpers/info/info.go
@@ -1,15 +1,69 @@
 package info
 
 import (
+	"fmt"
+	"io/ioutil"
 	"os"
 )
 
+const CA_PATH = "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
+const TOKEN_PATH = "/var/run/secrets/kubernetes.io/serviceaccount/token"
+const NS_PATH = "/var/run/secrets/kubernetes.io/serviceaccount/namespace"
+
 func GetNamespace() (string, error) {
-	// TODO if we're running in a pod is there a cool way to get the current namespace?
+	//If running in a pod use GetServiceAccountNS()
 	return os.Getenv("OSHINKO_CLUSTER_NAMESPACE"), nil
 }
 
 func GetSparkImage() (string, error) {
 	// TODO is there a good well-known location for a spark image?
 	return os.Getenv("OSHINKO_CLUSTER_IMAGE"), nil
+}
+
+func GetKubeConfigPath() string {
+	return os.Getenv("OSHINKO_KUBE_CONFIG")
+}
+
+func GetKubeProxyAddress() (string, error) {
+	proxyHost := os.Getenv("KUBERNETES_SERVICE_HOST")
+	if len(proxyHost) == 0 {
+		return "", fmt.Errorf("Unable to fetch KUBERNETES_SERVICE_HOST in Pod.")
+	}
+	return proxyHost, nil
+}
+
+func GetKubeProxyPort() (string, error) {
+	proxyPort := os.Getenv("KUBERNETES_SERVICE_PORT")
+	if len(proxyPort) == 0 {
+		return "", fmt.Errorf("Unable to fetch KUBERNETES_SERVICE_PORT in Pod.")
+	}
+	return proxyPort, nil
+}
+
+func GetServiceAccountCAPath() string {
+	return CA_PATH
+}
+
+func GetServiceAccountTokenPath() string {
+	return TOKEN_PATH
+}
+
+func GetServiceAccountNSPath() string {
+	return NS_PATH
+}
+
+func GetServiceAccountToken() ([]byte, error) {
+	token, err := ioutil.ReadFile(GetServiceAccountTokenPath())
+	if err != nil {
+		return nil, err
+	}
+	return token, err
+}
+
+func GetServiceAccountNS() ([]byte, error) {
+	namespace, err := ioutil.ReadFile(GetServiceAccountNSPath())
+	if err != nil {
+		return nil, err
+	}
+	return namespace, err
 }
