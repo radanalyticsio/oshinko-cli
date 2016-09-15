20f0a4baaee5e685f46625fddca165bbb8b4261c
michael mccune <msm@opbstudios.com>
2016-09-15 10:37:50 -0400

    update spark image usage (#69)
    This change makes the default spark image to be
    docker.io/radanalyticsio/openshift-spark. The script will now check to
    see if the specified image is available locally before attempting to tag
    and push it. Also some small cleanup for the default values.

--- tools/oshinko-deploy.sh
+++ tools/oshinko-deploy.sh
@@ -15,6 +15,9 @@
 # route IP -- address to use in the exposed route information
 # project name -- project to deploy oshinko into
 
+DEFAULT_SPARK_IMAGE=docker.io/radanalyticsio/openshift-spark
+DEFAULT_OPENSHIFT_USER=developer
+DEFAULT_OPENSHIFT_PROJECT=myproject
 
 while getopts :s:w:p:u:h opt; do
     case $opt in
@@ -38,9 +41,9 @@ while getopts :s:w:p:u:h opt; do
             echo "optional arguments:"
             echo "  -h            show this help message"
             echo "  -w HOSTNAME   hostname to use in exposed route to oshinko-web"
-            echo "  -s IMAGE      spark docker image to use for clusters (default: openshift-spark)"
-            echo "  -p PROJECT    OpenShift project name to install oshinko into (default: myproject)"
-            echo "  -u USER       OpenShift user to run commands as (default: developer)"
+            echo "  -s IMAGE      spark docker image to use for clusters (default: $DEFAULT_SPARK_IMAGE)"
+            echo "  -p PROJECT    OpenShift project name to install oshinko into (default: $DEFAULT_OPENSHIFT_USER)"
+            echo "  -u USER       OpenShift user to run commands as (default: $DEFAULT_OPENSHIFT_PROJECT)"
             echo
             echo "  If -w is not set, the default route will be used based on routing suffix, etc set at installation"
             exit
@@ -55,13 +58,18 @@ done
 if [ -z "$PROJECT" ]
 then
     echo "project name not set, using default value"
-    PROJECT=myproject
+    PROJECT=$DEFAULT_OPENSHIFT_PROJECT
 fi
 
 if [ -z "$OS_USER" ]
 then
     echo "user not set, using default value"
-    OS_USER=developer
+    OS_USER=$DEFAULT_OPENSHIFT_USER
+fi
+
+if [ -z "$SPARK_IMAGE" ]
+then
+    SPARK_IMAGE=$DEFAULT_SPARK_IMAGE
 fi
 
 oc login -u system:admin
@@ -87,11 +95,14 @@ docker push $REGISTRY/$PROJECT/oshinko-webui
 docker tag radanalytics-pyspark $REGISTRY/$PROJECT/radanalytics-pyspark
 docker push $REGISTRY/$PROJECT/radanalytics-pyspark
 
-if [ -z "$SPARK_IMAGE" ]; then
-    docker tag openshift-spark $REGISTRY/$PROJECT/oshinko-spark
-else
-    docker tag $SPARK_IMAGE $REGISTRY/$PROJECT/oshinko-spark
+# check to see if we have a local copy of the spark image
+# otherwise, pull it before tagging for oshinko
+local_spark_image=$(docker images -q $SPARK_IMAGE)
+if [ -z "$local_spark_image" ]
+then
+    docker pull $SPARK_IMAGE
 fi
+docker tag $SPARK_IMAGE $REGISTRY/$PROJECT/oshinko-spark
 docker push $REGISTRY/$PROJECT/oshinko-spark
 
 # set up the oshinko service account
