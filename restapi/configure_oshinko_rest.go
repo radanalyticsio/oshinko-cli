2e8e4ad42a0f188d31f9249ef65df8aeab5fecef
michael mccune <msm@opbstudios.com>
2016-10-13 10:25:10 -0400

    add commit hash to version (#86)
    * add commit hash to version
    
    This change will add the 7 digit commit hash to the tail of the tag name
    when it is computed by the build tools. It also changes the default git
    tag name to "unknown", this better reflects situations where the tags
    have not be pulled from the source repo.
    
    Changes
    * update common script library tagname function to reflect commit hashes
    
    * add a log message about server version
    
    This is another convenience to help with assuring which version of a
    server is installed.

--- restapi/configure_oshinko_rest.go
+++ restapi/configure_oshinko_rest.go
@@ -16,6 +16,7 @@ import (
 	"github.com/radanalyticsio/oshinko-rest/restapi/operations"
 	"github.com/radanalyticsio/oshinko-rest/restapi/operations/clusters"
 	"github.com/radanalyticsio/oshinko-rest/restapi/operations/server"
+	"github.com/radanalyticsio/oshinko-rest/version"
 )
 
 // This file is safe to edit. Once it exists it will not be overwritten
@@ -52,6 +53,8 @@ func configureAPI(api *operations.OshinkoRestAPI) http.Handler {
 	logging.Debug("Debug mode enabled")
 
 	api.Logger = logging.GetLogger().Printf
+
+	logging.GetLogger().Println("Starting", version.GetAppName(), "version", version.GetVersion())
 
 	return setupGlobalMiddleware(api.Serve(setupMiddlewares))
 }
--- tools/common.sh
+++ tools/common.sh
@@ -1,7 +1,9 @@
-TAG=`git describe --tags --abbrev=0 2> /dev/null | head -n1`
-if [ -z $TAG ]; then
-    TAG='0.0.0'
+GIT_TAG=`git describe --tags --abbrev=0 2> /dev/null | head -n1`
+if [ -z $GIT_TAG ]; then
+    GIT_TAG='unknown'
 fi
+GIT_COMMIT=`git log -n1 --pretty=format:%h`
+TAG="${GIT_TAG}-${GIT_COMMIT}"
 
 APP=oshinko-rest-server
 
