5a366deb118d274613e77188033664449f5d5518
Trevor McKay <tmckay@redhat.com>
2017-01-18 13:56:36 -0500

    Change return type of GetCommandGroups
    This way the caller doesn't have to care about the command group
    structure.

--- pkg/cmd/cli/extended.go
+++ pkg/cmd/cli/extended.go
@@ -11,12 +11,15 @@ import (
 	oshinkocmd "github.com/radanalyticsio/oshinko-cli/pkg/cmd/cli/cmd"
 )
 
-func GetCommandGroups(fullName string, f *clientcmd.Factory, in io.Reader, out io.Writer) templates.CommandGroups {
+func GetCommandGroups(fullName string, f *clientcmd.Factory, in io.Reader, out io.Writer) (
+											templates.CommandGroups,
+											*cobra.Command) {
+	first := oshinkocmd.NewCmdGet(fullName, f, in, out)
 	return templates.CommandGroups{
 		{
 			Message: "Basic Commands:",
 			Commands: []*cobra.Command{
-				oshinkocmd.NewCmdGet(fullName, f, in, out),
+				first,
 				oshinkocmd.NewCmdDelete(fullName, f, in, out),
 				oshinkocmd.NewCmdCreate(fullName, f, in, out),
 				oshinkocmd.NewCmdScale(fullName, f, in, out),
@@ -28,5 +31,5 @@ func GetCommandGroups(fullName string, f *clientcmd.Factory, in io.Reader, out i
 				oshinkocmd.NewCmdConfigMap(fullName, f, in, out),
 			},
 		},
-	}
+	}, first
 }
