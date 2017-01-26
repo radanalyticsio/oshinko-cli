6f8c867f597a6ca423556b50b0e110bff4462ea8
Trevor McKay <tmckay@redhat.com>
2017-01-26 11:36:25 -0500

    Update configs

--- scorpion_stare/make_stare_configs.sh
+++ scorpion_stare/make_stare_configs.sh
@@ -4,6 +4,10 @@ oc create configmap masterconfig --from-file=masterconfig
 
 oc create configmap workerconfig --from-file=workerconfig
 
+# This is a convenience so that if the driver application
+# is launched from s2i it can be specified in the app launch
+oc create configmap driverconfig --from-file=driverconfig
+
 oc create configmap clusterconfig --from-literal=metrics.enable=true \
                                   --from-literal=scorpionstare.enable=true \
                                   --from-literal=sparkmasterconfig=masterconfig \
