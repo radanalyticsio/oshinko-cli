3afee97fc09422ab785e6af2677093779e4d1038
Michael McCune <msm@redhat.com>
2017-02-17 12:13:06 -0500

    refactor common code in the client tests
    This change replaces some repeated code to make the tests more readable.

--- rest/tests/client/clusters_client_test.go
+++ rest/tests/client/clusters_client_test.go
@@ -32,6 +32,48 @@ func (checker *lessThanChecker) Check(params []interface{}, names []string) (res
 	return params[0].(int) < params[1].(int), ""
 }
 
+func checkClusterHelper(s *OshinkoRestTestSuite, c *check.C, params *clusters.FindSingleClusterParams, count int) (obsmcount int64, obswcount int64) {
+	const retries = 30
+	var tries int
+
+	for tries = 0; tries < retries; tries++ {
+		cldresult, err := s.cli.Clusters.FindSingleCluster(params)
+		if err != nil {
+			msg := fmt.Sprintf("%s \n %+v",
+				err.(*clusters.FindSingleClusterDefault).Error(),
+				err.(*clusters.FindSingleClusterDefault).Payload.Errors)
+			c.Fatal(msg)
+		}
+
+		// if enough pods are available, we loop through them and exit
+		// the retry loop, otherwise sleep for 1 second.
+		if len(cldresult.Payload.Cluster.Pods) >= count {
+			// loop through the pods to count workers and masters
+			obsmcount = 0
+			obswcount = 0
+			for _, pod := range cldresult.Payload.Cluster.Pods {
+				switch *pod.Type {
+				case "master":
+					if *pod.Status == "Running" {
+						obsmcount += 1
+					}
+				case "worker":
+					if *pod.Status == "Running" {
+						obswcount += 1
+					}
+				}
+			}
+			if int(obsmcount+obswcount) >= count {
+				break
+			}
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+	c.Assert(tries, LessThan, retries)
+	return
+}
+
 func (s *OshinkoRestTestSuite) TestCreateAndDeleteCluster(c *check.C) {
 	cname := "e2ecluster"
 	mcount := int64(1)
@@ -56,46 +98,9 @@ func (s *OshinkoRestTestSuite) TestCreateAndDeleteCluster(c *check.C) {
 	// must loop and try multiple times to read them. if we fail to read
 	// them after a set number of retries, we consider the test to have
 	// failed.
-	const retries = 30
-	var tries int
-	obsmcount, obswcount := int64(0), int64(0)
 	cldparams := clusters.NewFindSingleClusterParams().WithName(cname)
-	for tries = 0; tries < retries; tries++ {
-		cldresult, err := s.cli.Clusters.FindSingleCluster(cldparams)
-		if err != nil {
-			msg := fmt.Sprintf("%s \n %+v",
-				err.(*clusters.FindSingleClusterDefault).Error(),
-				err.(*clusters.FindSingleClusterDefault).Payload.Errors)
-			c.Fatal(msg)
-		}
+	obsmcount, obswcount := checkClusterHelper(s, c, cldparams, int(mcount+wcount))
 
-		// if enough pods are available, we loop through them and exit
-		// the retry loop, otherwise sleep for 1 second.
-		if len(cldresult.Payload.Cluster.Pods) >= int(mcount+wcount) {
-			// loop through the pods to count workers and masters
-			obsmcount = 0
-			obswcount = 0
-			for _, pod := range cldresult.Payload.Cluster.Pods {
-				switch *pod.Type {
-				case "master":
-					if *pod.Status == "Running" {
-						obsmcount += 1
-					}
-				case "worker":
-					if *pod.Status == "Running" {
-						obswcount += 1
-					}
-				}
-			}
-			if obsmcount+obswcount >= mcount+wcount {
-				break
-			}
-		}
-
-		time.Sleep(1 * time.Second)
-	}
-
-	c.Assert(tries, LessThan, retries)
 	c.Assert(obsmcount, check.Equals, mcount)
 	c.Assert(obswcount, check.Equals, wcount)
 
@@ -119,43 +124,8 @@ func (s *OshinkoRestTestSuite) TestCreateAndDeleteCluster(c *check.C) {
 	}
 
 	// check for update completion
-	obsmcount, obswcount = int64(0), int64(0)
-	for tries = 0; tries < retries; tries++ {
-		cldresult, err := s.cli.Clusters.FindSingleCluster(cldparams)
-		if err != nil {
-			msg := fmt.Sprintf("%s \n %+v",
-				err.(*clusters.FindSingleClusterDefault).Error(),
-				err.(*clusters.FindSingleClusterDefault).Payload.Errors)
-			c.Fatal(msg)
-		}
+	obsmcount, obswcount = checkClusterHelper(s, c, cldparams, int(mcount+uwcount))
 
-		// if enough pods are available, we loop through them and exit
-		// the retry loop, otherwise sleep for 1 second.
-		if len(cldresult.Payload.Cluster.Pods) >= int(mcount+uwcount) {
-			// loop through the pods to count workers and masters
-			obsmcount = 0
-			obswcount = 0
-			for _, pod := range cldresult.Payload.Cluster.Pods {
-				switch *pod.Type {
-				case "master":
-					if *pod.Status == "Running" {
-						obsmcount += 1
-					}
-				case "worker":
-					if *pod.Status == "Running" {
-						obswcount += 1
-					}
-				}
-			}
-			if obsmcount+obswcount >= mcount+uwcount {
-				break
-			}
-		}
-
-		time.Sleep(1 * time.Second)
-	}
-
-	c.Assert(tries, LessThan, retries)
 	c.Assert(obsmcount, check.Equals, mcount)
 	c.Assert(obswcount, check.Equals, uwcount)
 
@@ -172,6 +142,8 @@ func (s *OshinkoRestTestSuite) TestCreateAndDeleteCluster(c *check.C) {
 	}
 
 	// confirm delete
+	var tries int
+	const retries = 30
 	obsclcount := 0
 	for tries = 0; tries < retries; tries++ {
 		fcresult, err := s.cli.Clusters.FindClusters(nil)
