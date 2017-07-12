62d169b54a37ec627a5c513556b097e29ce840f6
Michael McCune <msm@redhat.com>
2017-07-12 15:20:52 -0400

    update vendor to latest 1.4 origin release
    This change updates the vendor directory to use the latest OpenShift
    Origin sources for the 1.4 release line. It also adds the Godeps.json
    file to the top level Godeps directory. This change has been added to
    help improve the quality of the dependency chain for the oshinko cli and
    rest applications.
    
    In addition to the vendor changes, the Godeps.json file has been
    modified by hand to include the dependencies for the rest server. These
    changes have been made by hand to account for difficulties with the
    godep tooling when evaluating the OpenShift code base. As OpenShift
    Origin uses modified upstreams in its vendor directory, these have been
    included in the oshinko vendor.
    
    The CONTRIBUTING.md file has also been updated to record the process
    used to create the vendor directory and the Godeps.json file.

--- vendor/github.com/go-openapi/spec/expander.go
+++ vendor/github.com/go-openapi/spec/expander.go
@@ -320,10 +320,12 @@ func ExpandSpec(spec *Swagger) error {
 	}
 
 	for key, defintition := range spec.Definitions {
-		if err := expandSchema(&defintition, "#/definitions/"+key, resolver); err != nil {
+		var def *Schema
+		var err error
+		if def, err = expandSchema(defintition, []string{"#/definitions/" + key}, resolver); err != nil {
 			return err
 		}
-		spec.Definitions[key] = defintition
+		spec.Definitions[key] = *def
 	}
 
 	for key, parameter := range spec.Parameters {
@@ -381,128 +383,133 @@ func ExpandSchema(schema *Schema, root interface{}, cache ResolutionCache) error
 		return err
 	}
 
-	if err := expandSchema(schema, "", resolver); err != nil {
+	refs := []string{""}
+	if rrr != nil {
+		refs[0] = rrr.String()
+	}
+	var s *Schema
+	if s, err = expandSchema(*schema, refs, resolver); err != nil {
 		return nil
 	}
+	*schema = *s
 	return nil
 }
 
-func expandItems(schema *Schema, parentRef string, resolver *schemaLoader) error {
-	if schema.Items != nil {
-		if schema.Items.Schema != nil {
-			sch := schema.Items.Schema
-			if err := expandSchema(sch, parentRef, resolver); err != nil {
-				return err
+func expandItems(target Schema, parentRefs []string, resolver *schemaLoader) (*Schema, error) {
+	if target.Items != nil {
+		if target.Items.Schema != nil {
+			t, err := expandSchema(*target.Items.Schema, parentRefs, resolver)
+			if err != nil {
+				return nil, err
 			}
+			*target.Items.Schema = *t
 		}
-		for i := range schema.Items.Schemas {
-			sch := &(schema.Items.Schemas[i])
-			if err := expandSchema(sch, parentRef, resolver); err != nil {
-				return err
+		for i := range target.Items.Schemas {
+			t, err := expandSchema(target.Items.Schemas[i], parentRefs, resolver)
+			if err != nil {
+				return nil, err
 			}
+			target.Items.Schemas[i] = *t
 		}
 	}
-
-	return nil
+	return &target, nil
 }
 
-func expandSchema(schema *Schema, parentRef string, resolver *schemaLoader) error {
-	if schema == nil {
-		return nil
+func expandSchema(target Schema, parentRefs []string, resolver *schemaLoader) (schema *Schema, err error) {
+	defer func() {
+		schema = &target
+	}()
+	if target.Ref.String() == "" && target.Ref.IsRoot() {
+		target = *resolver.root.(*Schema)
+		return
 	}
 
-	if schema.Ref.String() == "" && schema.Ref.IsRoot() {
-		*schema = *resolver.root.(*Schema)
-		return nil
+	// t is the new expanded schema
+	var t *Schema
+	for target.Ref.String() != "" {
+		// var newTarget Schema
+		pRefs := strings.Join(parentRefs, ",")
+		pRefs += ","
+		if strings.Contains(pRefs, target.Ref.String()+",") {
+			err = nil
+			return
+		}
+
+		if err = resolver.Resolve(&target.Ref, &t); err != nil {
+			return
+		}
+		parentRefs = append(parentRefs, target.Ref.String())
+		target = *t
 	}
 
-	if schema.Ref.String() != "" {
-		currentSchema := *schema
-		for currentSchema.Ref.String() != "" {
-			var newSchema Schema
-			if currentSchema.Ref.String() == parentRef {
-				break
+	if t, err = expandItems(target, parentRefs, resolver); err != nil {
+		return
+	}
+	target = *t
+
+	for i := range target.AllOf {
+		if t, err = expandSchema(target.AllOf[i], parentRefs, resolver); err != nil {
+			return
+		}
+		target.AllOf[i] = *t
+	}
+	for i := range target.AnyOf {
+		if t, err = expandSchema(target.AnyOf[i], parentRefs, resolver); err != nil {
+			return
+		}
+		target.AnyOf[i] = *t
+	}
+	for i := range target.OneOf {
+		if t, err = expandSchema(target.OneOf[i], parentRefs, resolver); err != nil {
+			return
+		}
+		target.OneOf[i] = *t
+	}
+	if target.Not != nil {
+		if t, err = expandSchema(*target.Not, parentRefs, resolver); err != nil {
+			return
+		}
+		*target.Not = *t
+	}
+	for k, _ := range target.Properties {
+		if t, err = expandSchema(target.Properties[k], parentRefs, resolver); err != nil {
+			return
+		}
+		target.Properties[k] = *t
+	}
+	if target.AdditionalProperties != nil && target.AdditionalProperties.Schema != nil {
+		if t, err = expandSchema(*target.AdditionalProperties.Schema, parentRefs, resolver); err != nil {
+			return
+		}
+		*target.AdditionalProperties.Schema = *t
+	}
+	for k, _ := range target.PatternProperties {
+		if t, err = expandSchema(target.PatternProperties[k], parentRefs, resolver); err != nil {
+			return
+		}
+		target.PatternProperties[k] = *t
+	}
+	for k, _ := range target.Dependencies {
+		if target.Dependencies[k].Schema != nil {
+			if t, err = expandSchema(*target.Dependencies[k].Schema, parentRefs, resolver); err != nil {
+				return
 			}
-
-			if err := resolver.Resolve(&currentSchema.Ref, &newSchema); err != nil {
-				return err
-			}
-			parentRef = currentSchema.Ref.String()
-
-			currentSchema = newSchema
-		}
-		*schema = currentSchema
-	}
-
-	if err := expandItems(schema, parentRef, resolver); err != nil {
-		return err
-	}
-
-	for i := range schema.AllOf {
-		sch := &(schema.AllOf[i])
-		if err := expandSchema(sch, parentRef, resolver); err != nil {
-			return err
+			*target.Dependencies[k].Schema = *t
 		}
 	}
-	for i := range schema.AnyOf {
-		sch := &(schema.AnyOf[i])
-		if err := expandSchema(sch, parentRef, resolver); err != nil {
-			return err
+	if target.AdditionalItems != nil && target.AdditionalItems.Schema != nil {
+		if t, err = expandSchema(*target.AdditionalItems.Schema, parentRefs, resolver); err != nil {
+			return
 		}
+		*target.AdditionalItems.Schema = *t
 	}
-	for i := range schema.OneOf {
-		sch := &(schema.OneOf[i])
-		if err := expandSchema(sch, parentRef, resolver); err != nil {
-			return err
+	for k, _ := range target.Definitions {
+		if t, err = expandSchema(target.Definitions[k], parentRefs, resolver); err != nil {
+			return
 		}
+		target.Definitions[k] = *t
 	}
-	if schema.Not != nil {
-		sch := schema.Not
-		if err := expandSchema(sch, parentRef, resolver); err != nil {
-			return err
-		}
-	}
-	for k, v := range schema.Properties {
-		if err := expandSchema(&v, parentRef, resolver); err != nil {
-			return err
-		}
-		schema.Properties[k] = v
-	}
-	if schema.AdditionalProperties != nil && schema.AdditionalProperties.Schema != nil {
-		sch := schema.AdditionalProperties.Schema
-		if err := expandSchema(sch, parentRef, resolver); err != nil {
-			return err
-		}
-	}
-	for k, v := range schema.PatternProperties {
-		vp := &v
-		if err := expandSchema(vp, parentRef, resolver); err != nil {
-			return err
-		}
-		schema.PatternProperties[k] = *vp
-	}
-	for k, v := range schema.Dependencies {
-		if v.Schema != nil {
-			sch := v.Schema
-			if err := expandSchema(sch, parentRef, resolver); err != nil {
-				return err
-			}
-			schema.Dependencies[k] = v
-		}
-	}
-	if schema.AdditionalItems != nil && schema.AdditionalItems.Schema != nil {
-		sch := schema.AdditionalItems.Schema
-		if err := expandSchema(sch, parentRef, resolver); err != nil {
-			return err
-		}
-	}
-	for k, v := range schema.Definitions {
-		if err := expandSchema(&v, parentRef, resolver); err != nil {
-			return err
-		}
-		schema.Definitions[k] = v
-	}
-	return nil
+	return
 }
 
 func expandPathItem(pathItem *PathItem, resolver *schemaLoader) error {
@@ -582,12 +589,14 @@ func expandResponse(response *Response, resolver *schemaLoader) error {
 	}
 
 	if response.Schema != nil {
-		parentRef := response.Schema.Ref.String()
+		parentRefs := []string{response.Schema.Ref.String()}
 		if err := resolver.Resolve(&response.Schema.Ref, &response.Schema); err != nil {
 			return err
 		}
-		if err := expandSchema(response.Schema, parentRef, resolver); err != nil {
+		if s, err := expandSchema(*response.Schema, parentRefs, resolver); err != nil {
 			return err
+		} else {
+			*response.Schema = *s
 		}
 	}
 	return nil
@@ -603,12 +612,14 @@ func expandParameter(parameter *Parameter, resolver *schemaLoader) error {
 		}
 	}
 	if parameter.Schema != nil {
-		parentRef := parameter.Schema.Ref.String()
+		parentRefs := []string{parameter.Schema.Ref.String()}
 		if err := resolver.Resolve(&parameter.Schema.Ref, &parameter.Schema); err != nil {
 			return err
 		}
-		if err := expandSchema(parameter.Schema, parentRef, resolver); err != nil {
+		if s, err := expandSchema(*parameter.Schema, parentRefs, resolver); err != nil {
 			return err
+		} else {
+			*parameter.Schema = *s
 		}
 	}
 	return nil
--- vendor/github.com/openshift/origin/test/end-to-end/core.sh
+++ vendor/github.com/openshift/origin/test/end-to-end/core.sh
@@ -106,6 +106,17 @@ os::cmd::expect_success "curl -f http://${DOCKER_REGISTRY}/healthz"
 
 os::cmd::expect_success "dig @${API_HOST} docker-registry.default.local. A"
 
+os::log::info "Verify that an image based on a remote image can be pushed to the same image stream while pull-through enabled."
+os::cmd::expect_success "oc login -u user0 -p pass"
+os::cmd::expect_success "oc new-project project0"
+# An image stream will be created that will track the source image and the resulting image
+# will be pushed to image stream "busybox:latest"
+os::cmd::expect_success "oc new-build -D \$'FROM busybox:glibc\nRUN echo abc >/tmp/foo'"
+os::cmd::try_until_text "oc get build/busybox-1 -o 'jsonpath={.metadata.name}'" "busybox-1" "$(( 10*TIME_MIN ))"
+os::cmd::try_until_text "oc get build/busybox-1 -o 'jsonpath={.status.phase}'" '^(Complete|Failed|Error)$' "$(( 10*TIME_MIN ))"
+os::cmd::expect_success_and_text "oc get build/busybox-1 -o 'jsonpath={.status.phase}'" 'Complete'
+os::cmd::expect_success "oc get istag/busybox:latest"
+
 # Client setup (log in as e2e-user and set 'test' as the default project)
 # This is required to be able to push to the registry!
 os::log::info "Logging in as a regular user (e2e-user:pass) with project 'test'..."
@@ -310,7 +321,6 @@ os::cmd::expect_success 'oc policy add-role-to-user system:image-pusher pusher -
 os::log::info "Docker cross-repo mount"
 os::cmd::expect_success_and_text "curl -I -X HEAD -u 'pusher:${pusher_token}' '${DOCKER_REGISTRY}/v2/cache/ruby-22-centos7/blobs/$rubyimageblob'" "200 OK"
 os::cmd::try_until_text "oc get -n custom is/ruby-22-centos7 -o 'jsonpath={.status.tags[*].tag}'" "latest" $((20*TIME_SEC))
-os::cmd::expect_success_and_text "curl -I -X HEAD -u 'pusher:${pusher_token}' '${DOCKER_REGISTRY}/v2/custom/ruby-22-centos7/blobs/$rubyimageblob'" "200 OK"
 os::cmd::try_until_text "oc policy can-i update imagestreams/layers -n crossmount '--token=${pusher_token}'" "yes"
 os::cmd::expect_success_and_text "curl -I -X HEAD -u 'pusher:${pusher_token}' '${DOCKER_REGISTRY}/v2/crossmount/repo/blobs/$rubyimageblob'" "404 Not Found"
 # 202 means that cross-repo mount has failed (in this case because of blob doesn't exist in the source repository), client needs to reupload the blob
