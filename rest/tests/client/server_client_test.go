15ef303a6c8b1319d75cb28f4dae985b1dc49bd2
Trevor McKay <tmckay@redhat.com>
2017-09-08 15:13:54 -0400

    Expand server test to check for reported cluster image

--- rest/tests/client/server_client_test.go
+++ rest/tests/client/server_client_test.go
@@ -4,17 +4,33 @@ import (
 	check "gopkg.in/check.v1"
 
 	"github.com/radanalyticsio/oshinko-cli/rest/version"
+	"github.com/radanalyticsio/oshinko-cli/rest/helpers/info"
+	"os"
 )
 
 func (s *OshinkoRestTestSuite) TestServerInfo(c *check.C) {
 	resp, _ := s.cli.Server.GetServerInfo(nil)
 
+	val := os.Getenv("OSHINKO_CLUSTER_IMAGE")
+	os.Setenv("OSHINKO_CLUSTER_IMAGE", "")
+
 	expectedName := version.GetAppName()
 	expectedVersion := version.GetVersion()
+	expectedImage := info.GetSparkImage()
 
 	observedName := resp.Payload.Application.Name
 	observedVersion := resp.Payload.Application.Version
+	observedImage := resp.Payload.Application.DefaultClusterImage
 
 	c.Assert(*observedName, check.Equals, expectedName)
 	c.Assert(*observedVersion, check.Equals, expectedVersion)
+	c.Assert(*observedImage, check.Equals, expectedImage)
+
+	os.Setenv("OSHINKO_CLUSTER_IMAGE", "bobby")
+	expectedImage = "bobby"
+	resp, _ = s.cli.Server.GetServerInfo(nil)
+	observedImage = resp.Payload.Application.DefaultClusterImage
+	c.Assert(*observedImage, check.Equals, expectedImage)
+
+	os.Setenv("OSHINKO_CLUSTER_IMAGE", val)
 }
