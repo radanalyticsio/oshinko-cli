482777d760d4cae7719acd2a3f7f058c0a0eee25
Trevor McKay <tmckay@redhat.com>
2017-09-18 16:13:14 -0400

    Move release-templates.sh to tools

--- rest/tools/release-templates.sh
+++ rest/tools/release-templates.sh
@@ -5,11 +5,11 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
-TOP_DIR=$(readlink -f `dirname "$0"` | grep -o '.*/oshinko-cli')/rest
+TOP_DIR=$(readlink -f `dirname "$0"` | grep -o '.*/oshinko-cli')/rest/tools
 mkdir -p $TOP_DIR/release_templates
 rm -rf $TOP_DIR/release_templates/*
 
-cp $TOP_DIR/tools/server-ui-template.yaml $TOP_DIR/release_templates
+cp $TOP_DIR/server-ui-template.yaml $TOP_DIR/release_templates
 
 sed -r -i "s@(radanalyticsio/oshinko-.*)@\1:$1@" $TOP_DIR/release_templates/*
 
